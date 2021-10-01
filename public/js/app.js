39009bccf17294c5c750bab559f05706822d3c28
ubuntu <ubuntu@localhost.localdomain>
2021-10-01 16:38:32 +0800

    fixing some bugs

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,3 @@
-
-
-
-
-
-// const placeName = 'Xiamen'
-// const geocodeURL ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(placeName) + '.json?access_token=pk.eyJ1IjoiZG93aGlsZTMiLCJhIjoiY2t0czRteXpjMWN1NTJ2cG1sYnA4ZG5xcyJ9.baSDEdf10IKkP-aho53Z7w&limit=1'
-
-// fetch(geocodeURL).then((response) => {
-//     response.json().then((data) => {
-//         if (data.features.length === 0) {
-//             return console.log('No such place.')
-//         }
-
-//         const latitude = data.features[0].center[1]
-//         const longitude = data.features[0].center[0]
-//         const coordinates = latitude + ',' + longitude
-//         const weatherURL = "http://api.weatherstack.com/current?access_key=fd5b0e0d2123158734707f00d4638660&query="
-//                        + coordinates + '&units=m'
-//         fetch(weatherURL).then((response) => {
-//             response.json().then((data) => {
-//                 console.log("Address: " + placeName)
-//                 console.log("Forecast: " + data.current.weather_descriptions[0])
-//                 console.log("Temperature: ", data.current.temperature)
-//             })
-//         })
-//     })
-// })
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -63,3 +32,31 @@ weatherForm.addEventListener('submit', (e) => {
     })
 
 })
+
+
+// const placeName = 'Xiamen'
+// const geocodeURL ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(placeName) + '.json?access_token=pk.eyJ1IjoiZG93aGlsZTMiLCJhIjoiY2t0czRteXpjMWN1NTJ2cG1sYnA4ZG5xcyJ9.baSDEdf10IKkP-aho53Z7w&limit=1'
+
+// fetch(geocodeURL).then((response) => {
+//     response.json().then((data) => {
+//         if (data.features.length === 0) {
+//             return console.log('No such place.')
+//         }
+
+//         const latitude = data.features[0].center[1]
+//         const longitude = data.features[0].center[0]
+//         const coordinates = latitude + ',' + longitude
+//         const weatherURL = "http://api.weatherstack.com/current?access_key=fd5b0e0d2123158734707f00d4638660&query="
+//                        + coordinates + '&units=m'
+//         fetch(weatherURL).then((response) => {
+//             response.json().then((data) => {
+//                 console.log("Address: " + placeName)
+//                 console.log("Forecast: " + data.current.weather_descriptions[0])
+//                 console.log("Temperature: ", data.current.temperature)
+//             })
+//         })
+//     })
+// })
+
+
+
